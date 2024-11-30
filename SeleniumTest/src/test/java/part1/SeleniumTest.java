cb657a9d9461db8f47e01b915213c677f5038ba2
ekinalaydin <ekinaly06@gmail.com>
2024-11-30 22:49:59 +0300

    Selenium test

---
+++ SeleniumTest/src/test/java/part1/SeleniumTest.java
@@ -0,0 +1,46 @@
+package part1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    WebDriver driver;
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+    @AfterClass
+    public void tearDown() {
+//        driver.quit();
+
+    }
+    @Test
+    public void testLoginApplication() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement username = driver.findElement(By.name("username"));
+        //HTML element has interface
+        username.sendKeys("Admin");
+
+        var password = driver.findElement(By.name("password"));
+        //java keyword -- datatype based on driver.findElement
+        password.sendKeys("admin123");
+
+        driver.findElement(By.tagName("button")).click();
+        Thread.sleep(2000);
+        String actualResult = driver.findElement(By.tagName("h6")).getText();
+        String expectedResult = "Dashboard";
+        Assert.assertEquals(actualResult, expectedResult);
+        //testin dogrulugunu kontrol etmek icin
+    }
+
+}
+
